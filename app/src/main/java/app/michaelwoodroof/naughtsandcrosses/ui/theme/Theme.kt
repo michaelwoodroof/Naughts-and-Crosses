735109932c6b147a5a2b29b4bdda25a265f33ab9
Michael Woodroof <michaelwoodroof@outlook.com>
2023-07-14 10:24:14 +0100

    fix(tidy): General tidy of the code

--- app/src/main/java/app/michaelwoodroof/naughtsandcrosses/ui/theme/Theme.kt
+++ app/src/main/java/app/michaelwoodroof/naughtsandcrosses/ui/theme/Theme.kt
@@ -1,19 +1,15 @@
 package app.michaelwoodroof.naughtsandcrosses.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 /**
  * Defines the light colour scheme that the app follows
@@ -100,10 +96,11 @@ fun NaughtsAndCrossesTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
-            (view.context as Activity).window.navigationBarColor = colorScheme.background.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !darkTheme
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightNavigationBars = !darkTheme
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
         }
     }
 
